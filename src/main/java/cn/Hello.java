199c284ddab42f7b896aca3e3a15dfcc0801ba94
jiba946 <46842713+jiba946@users.noreply.github.com>
2019-01-20 15:18:21 +0800

    Update Hello.java

--- src/main/java/cn/Hello.java
+++ src/main/java/cn/Hello.java
@@ -2,7 +2,7 @@ package cn;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("11111111");
-        System.out.println("11111111");
+        System.out.println("11111115551");
+        
     }
 }
